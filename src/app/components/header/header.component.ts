b7f9c09168619fc7bf427c24b9b34144da8f3a31
ostap23 <69ostap18s@gmail.com>
2021-06-18 17:00:23 +0300

    added title

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,39 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  name: string = 'Ivan';
-  count: number = 0;
-  isVisible: boolean = false;
-  car = {
-    mark: 'Hyundai',
-    model: 'Tucson'
-  };
-
-  allUsers = ['Petro', 'Oksana', 'Alina'];
-  pageUrl = 'https://www.google.com';
-  pageName = 'Google';
-  message: string = '';
-  login: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  printFullCar(): string {
-    return `${this.car.mark} ${this.car.model}`
-  }
-
-  showMessage(event: MouseEvent): void {
-    console.log('event', event);
-    
-
-    this.message = 'Hello Angular';
-  }
-
-  getLogin(): void {
-    console.log(this.login);
-    
-  }
 
 }
